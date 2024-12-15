3e5a72520e30fe61c65d05c338f2eeead421372d
vthang0412 <vthang041206@gmail.com>
2024-12-16 00:59:09 +0700

    ss16-ex1.cpp

--- ss16-ex1.cpp
+++ ss16-ex1.cpp
@@ -4,7 +4,9 @@ int main(){
 	int number=5;
 	int* numberPtr=&number;
 	printf("%d\n",&number); 
-    printf("%d\n",*numberPtr);
+   	printf("%d\n",numberPtr);
+	printf("%d\n",number); 
+   	printf("%d\n",*numberPtr);
 	return 0;
 
 }
